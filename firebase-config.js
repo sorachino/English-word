a5dd08a2d8f1ef4fcd3345513720a0997251904f
sorachino <124219970+sorachino@users.noreply.github.com>
2026-08-15 21:41:53 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -6,11 +6,11 @@
 // 問題なく動きます。ランキング機能だけが「設定が必要です」と表示されます。
 
 const FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBqRmq3K9VZJWTpZlWpNFMIaUF8LJp1IvQ",
+  authDomain: "english-word-83c2a.firebaseapp.com",
+  databaseURL: "https://english-word-83c2a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "english-word-83c2a",
+  storageBucket: "english-word-83c2a.firebasestorage.app",
+  messagingSenderId: "515435517015",
+  appId: "1:515435517015:web:0675c37cbe4958e5e57f12"
 };
